e4fe54218fa5cdd3bbad8d8353d7d7d1e5b70ba5
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-07-14 20:42:31 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    index = 0
-    for (let Ycord = 0; Ycord <= randint(1, 128); Ycord++) {
+    Ycord = randint(1, 128)
+    for (let index = 0; index <= 160; index++) {
         Xcord = index
         LCD1IN8.DrawPoint(
         Xcord,
@@ -15,11 +15,11 @@ input.onGesture(Gesture.Shake, function () {
     LCD1IN8.LCD_Clear()
     LCD1IN8.LCD_Display()
 })
-let index = 0
+let Ycord = 0
 let Xcord = 0
 let lite = 9
 Xcord = 1
-let Ycord = 1
+Ycord = 1
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Clear()
 LCD1IN8.LCD_Display()
