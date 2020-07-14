ac2e2b3a6963b37cb1cedf358f3696af3c323433
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-07-14 20:33:08 +0100

    change loop to draw vertical ( Ycord) bar & clear screen when MBit shaken

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 160; index++) {
-        Xcord = index
+    for (let index = 0; index <= 128; index++) {
+        Ycord = index
         LCD1IN8.DrawPoint(
-        Xcord,
-        Ycord + 32,
+        80,
+        Ycord,
         LCD1IN8.Get_Color(LCD_COLOR.BLUE),
         DOT_PIXEL.DOT_PIXEL_1
         )
-        LCD1IN8.LCD_Display()
     }
+    LCD1IN8.LCD_Display()
+})
+input.onGesture(Gesture.Shake, function () {
+    LCD1IN8.LCD_Clear()
+    LCD1IN8.LCD_Display()
 })
 let Ycord = 0
-let Xcord = 0
 let lite = 32
-Xcord = 1
+let Xcord = 1
 Ycord = 1
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Clear()
